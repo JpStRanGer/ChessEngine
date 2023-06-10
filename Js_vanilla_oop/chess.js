7d15b825a94fb181857e2591c1ffbc205e468d96
Jonas Pettersen <jonas.st.pettersen@gmail.com>
2023-06-10 21:42:25 +0200

    Feat(getSquareName): added function for returning square name

--- Js_vanilla_oop/chess.js
+++ Js_vanilla_oop/chess.js
@@ -4,9 +4,13 @@ let context = canvas.getContext("2d");
 class Chessboard {
     constructor() {
         console.log("constructed obj Chessboard");
-        this.frameSize = 50;
+        this.frameSize = 90;
         this.boardSize = 8;
-        this.squareColor = ["#7a8285","#222"]
+        this.squareColor = ["#7a8285", "#222"];
+        this.boardIndex = {
+            horizontal: ["a", "b", "c", "d", "e", "f", "h", "g"],
+            vertical: ["1", "2", "3", "4", "5", "6", "7", "8"],
+        };
         this.bordWidth = canvas.width - this.frameSize * 2;
         this.bordHeight = this.bordWidth;
         this.squareSize = this.bordWidth / this.boardSize;
@@ -17,45 +21,52 @@ class Chessboard {
     draw() {
         console.log("draw Chessboard");
 
-        this.drawChessboard(this.boardSize, this.squareSize, this.frameSize);
+        this.drawChessboard(
+            this.boardSize,
+            this.squareSize,
+            this.frameSize,
+            this.boardIndex
+        );
     }
 
-    drawChessboard(boardSize, squareSize, frameSize) {
+    drawChessboard(boardSize, squareSize, frameSize, boardIndex) {
         // Draw frame
-        let textOfset = 13;
-        let boardIndex = {
-            horizontal: ["a", "b", "c", "d", "e", "f", "h", "g"],
-            vertical: ["1", "2", "3", "4", "5", "6", "7", "8"],
-        };
+        let textOfset = 33;
+
+        let collomnPos = frameSize + squareSize / 2;
+
         for (var i = 0; i < boardSize; i++) {
             context.font = "bold 30px serif";
             context.textAlign = "center";
             context.textBaseline = "middle";
             context.fillText(
                 boardIndex.horizontal[i],
-                frameSize + squareSize / 2 + i * squareSize,
+                collomnPos + i * squareSize,
                 frameSize - textOfset
             );
             context.fillText(
                 boardIndex.horizontal[i],
-                frameSize + squareSize / 2 + i * squareSize,
+                collomnPos + i * squareSize,
                 frameSize + this.bordHeight + textOfset
             );
             context.fillText(
                 8 - i,
-                frameSize - 10,
-                frameSize + squareSize / 2 + i * squareSize
+                frameSize - textOfset,
+                collomnPos + i * squareSize
             );
             context.fillText(
                 boardIndex.vertical[7 - i],
-                frameSize + this.bordHeight + 25,
-                frameSize + squareSize / 2 + i * squareSize
+                frameSize + this.bordHeight + textOfset,
+                collomnPos + i * squareSize
             );
         }
         // Draw board
         for (var row = 0; row < boardSize; row++) {
             for (var col = 0; col < boardSize; col++) {
-                context.fillStyle = (row + col) % 2 === 0 ? this.squareColor[0] : this.squareColor[1];
+                context.fillStyle =
+                    (row + col) % 2 === 0
+                        ? this.squareColor[0]
+                        : this.squareColor[1];
                 context.fillRect(
                     frameSize + row * squareSize,
                     frameSize + col * squareSize,
@@ -66,11 +77,27 @@ class Chessboard {
         }
     }
 
-    showDebugger() {}
+    getSquareName(row, col) {
+        if (col < 1 || row < 1) {
+            console.log("out of range");
+            return null;
+        }
+        if (col > 8 || row > 8) {
+            console.log("out of range");
+            return null;
+        }
+        console.log(
+            row - 1,
+            col - 1,
+            this.boardIndex.horizontal[col - 1] + this.boardIndex.vertical[8-row]
+        );
+        return this.boardIndex.horizontal[col - 1] + this.boardIndex.vertical[8 - row];
+    }
 }
 
 class Debugger {
     constructor(chessboard) {
+        this._chessboard = chessboard;
         this.showDebugger = true;
         if (this.showDebugger) {
             canvas.addEventListener("mousemove", this.mouseTracker.bind(this));
@@ -79,7 +106,8 @@ class Debugger {
     }
 
     drawOnDisplay(debugData) {
-        let debugHeight = 300;
+        let fontSize = 20;
+        let debugHeight = debugData.length * fontSize;
         let debugWidth = 300;
 
         // Draw debug window
@@ -93,7 +121,7 @@ class Debugger {
 
         // Draw debug text
         context.fillStyle = "black";
-        context.font = "bold 30px serif";
+        context.font = `bold ${fontSize}px serif`;
         context.textAlign = "left";
         context.textBaseline = "top";
         let lines = debugData.length;
@@ -110,6 +138,12 @@ class Debugger {
 
     mouseTracker(event) {
         let boxPosition = canvas.getBoundingClientRect();
+        let boxX = event.clientX - boxPosition.left;
+        let boxY = event.clientY - boxPosition.top;
+        let squareSize = this._chessboard.squareSize;
+        let row = Math.floor(boxY / squareSize);
+        let col = Math.floor(boxX / squareSize);
+        let squareName = this._chessboard.getSquareName(row, col);
         let debugData = [
             // `Font ${context.font}`,
             `canvas.width ${canvas.width}`,
@@ -120,11 +154,12 @@ class Debugger {
             `event.clientY: ${event.clientY}`,
             `boxX: ${event.clientX - boxPosition.left}`,
             `boxY: ${event.clientY - boxPosition.top}`,
+            `Square: ${squareName}`,
         ];
         this.drawOnDisplay(debugData);
     }
 }
 
-const chessboard = new Chessboard();
+const chessboardw = new Chessboard();
 
-const appDebugger = new Debugger();
+const appDebugger = new Debugger(chessboardw);

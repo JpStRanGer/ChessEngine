29d8ce83ece1dd40c9abf026fe73c87f01411758
Jonas Pettersen <jonas.st.pettersen@gmail.com>
2023-06-10 20:10:03 +0200

    Feat(drawChessboard): made indexes and separated board from canvas.

--- Js_vanilla_oop/chess.js
+++ Js_vanilla_oop/chess.js
@@ -4,8 +4,12 @@ let context = canvas.getContext("2d");
 class Chessboard {
     constructor() {
         console.log("constructed obj Chessboard");
+        this.frameSize = 50;
         this.boardSize = 8;
-        this.squareSize = canvas.width / 8;
+        this.squareColor = ["#7a8285","#222"]
+        this.bordWidth = canvas.width - this.frameSize * 2;
+        this.bordHeight = this.bordWidth;
+        this.squareSize = this.bordWidth / this.boardSize;
 
         this.draw();
     }
@@ -13,14 +17,50 @@ class Chessboard {
     draw() {
         console.log("draw Chessboard");
 
-        for (var row = 0; row < this.squareSize; row++) {
-            for (var col = 0; col < this.boardSize; col++) {
-                context.fillStyle = (row + col) % 2 === 0 ? "red" : "blue";
+        this.drawChessboard(this.boardSize, this.squareSize, this.frameSize);
+    }
+
+    drawChessboard(boardSize, squareSize, frameSize) {
+        // Draw frame
+        let textOfset = 13;
+        let boardIndex = {
+            horizontal: ["a", "b", "c", "d", "e", "f", "h", "g"],
+            vertical: ["1", "2", "3", "4", "5", "6", "7", "8"],
+        };
+        for (var i = 0; i < boardSize; i++) {
+            context.font = "bold 30px serif";
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillText(
+                boardIndex.horizontal[i],
+                frameSize + squareSize / 2 + i * squareSize,
+                frameSize - textOfset
+            );
+            context.fillText(
+                boardIndex.horizontal[i],
+                frameSize + squareSize / 2 + i * squareSize,
+                frameSize + this.bordHeight + textOfset
+            );
+            context.fillText(
+                8 - i,
+                frameSize - 10,
+                frameSize + squareSize / 2 + i * squareSize
+            );
+            context.fillText(
+                boardIndex.vertical[7 - i],
+                frameSize + this.bordHeight + 25,
+                frameSize + squareSize / 2 + i * squareSize
+            );
+        }
+        // Draw board
+        for (var row = 0; row < boardSize; row++) {
+            for (var col = 0; col < boardSize; col++) {
+                context.fillStyle = (row + col) % 2 === 0 ? this.squareColor[0] : this.squareColor[1];
                 context.fillRect(
-                    row * this.squareSize,
-                    col * this.squareSize,
-                    this.squareSize,
-                    this.squareSize
+                    frameSize + row * squareSize,
+                    frameSize + col * squareSize,
+                    squareSize,
+                    squareSize
                 );
             }
         }
@@ -33,13 +73,16 @@ class Debugger {
     constructor(chessboard) {
         this.showDebugger = true;
         if (this.showDebugger) {
-            this.drawOnDisplay();
+            canvas.addEventListener("mousemove", this.mouseTracker.bind(this));
+            // this.drawOnDisplay();
         }
     }
 
-    drawOnDisplay() {
-        let debugHeight = 100;
+    drawOnDisplay(debugData) {
+        let debugHeight = 300;
         let debugWidth = 300;
+
+        // Draw debug window
         context.fillStyle = "gray";
         context.fillRect(
             0,
@@ -48,24 +91,37 @@ class Debugger {
             debugHeight
         );
 
+        // Draw debug text
         context.fillStyle = "black";
         context.font = "bold 30px serif";
+        context.textAlign = "left";
         context.textBaseline = "top";
-        let lines = 3;
-        let lineHeight = debugHeight/lines;
-        let debugData = [
-            `Font ${context.font}`,
-            `canvas.width ${canvas.width}`,
-            `canvas.height ${canvas.height}`,
-        ]
-        for (var line = 0; line < lines; line++) {
+        let lines = debugData.length;
+        let lineHeight = debugHeight / lines;
+        for (var line = 0; line < debugData.length; line++) {
             context.fillText(
                 debugData[line],
-                10,
-                canvas.height - debugHeight + 5 + lineHeight*line,
+                0,
+                canvas.height - debugHeight + 1 + lineHeight * line,
                 debugWidth
             );
         }
+    }
+
+    mouseTracker(event) {
+        let boxPosition = canvas.getBoundingClientRect();
+        let debugData = [
+            // `Font ${context.font}`,
+            `canvas.width ${canvas.width}`,
+            `canvas.height ${canvas.height}`,
+            `boxPosition.left: ${boxPosition.left}`,
+            `boxPosition.top: ${boxPosition.top}`,
+            `event.clientX: ${event.clientX}`,
+            `event.clientY: ${event.clientY}`,
+            `boxX: ${event.clientX - boxPosition.left}`,
+            `boxY: ${event.clientY - boxPosition.top}`,
+        ];
+        this.drawOnDisplay(debugData);
     }
 }
 
